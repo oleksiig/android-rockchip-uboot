61bcbb023f3cc32e829ad308143d5270e5f66014
Oleksii Gulchenko <alexey.gulchenko@gmail.com>
2019-09-09 18:57:42 +0300

    x3399evk.h: Added initial GPT partitions for Android support
    Signed-off-by: Oleksii Gulchenko <alexey.gulchenko@gmail.com>

--- include/configs/x3399evk.h
+++ include/configs/x3399evk.h
@@ -47,13 +47,30 @@
 
 #ifndef CONFIG_SPL_BUILD
 
-#define ENV_MEM_LAYOUT_SETTINGS \
+#define CONFIG_EXTRA_ENV_SETTINGS \
     "fdt_addr_r=0x01f00000\0" \
     "kernel_addr_r=0x02080000\0" \
-    "ramdisk_addr_r=0x04000000\0"
-
-#define CONFIG_EXTRA_ENV_SETTINGS \
-    "extraenv=none\0"
+    "ramdisk_addr_r=0x04000000\0" \
+    "uuid_gpt_disk=5DE6B69D-5FFA-4CEA-97DE-A5F287DB3B44\0" \
+    "uuid_gpt_boot=F4C2B80D-FFD1-4D76-A63C-FF56ACA2F3B0\0" \
+    "uuid_gpt_recovery=43E69039-4608-4591-8C92-FF5758DB76E6\0" \
+    "uuid_gpt_system=CF16C17A-7935-421F-80AC-BD870520E000\0" \
+    "uuid_gpt_vendor=7C57E557-063D-485D-B9F1-A3A7B3614901\0" \
+    "uuid_gpt_cache=850FCD31-6A0D-44C4-848B-4781879301F1\0" \
+    "uuid_gpt_misc=E4AD2B83-18C9-4C08-8DD6-99B3C80598C9\0" \
+    "uuid_gpt_frp=888FCC59-A68E-4BA3-8D52-7F81628F5147\0" \
+    "uuid_gpt_metadata=75028084-CF5A-4191-9A1B-E208FC399DC2\0" \
+    "uuid_gpt_userdata=196D4D41-83B5-4B2C-899D-02FE19D27BCA\0" \
+    "partitions=uuid_disk=${uuid_gpt_disk};" \
+        "name=boot,start=32M,size=32M,uuid=${uuid_gpt_boot};" \
+        "name=recovery,size=32MB,uuid=${uuid_gpt_recovery};" \
+        "name=system,size=2G,uuid=${uuid_gpt_system};" \
+        "name=vendor,size=256M,uuid=${uuid_gpt_vendor};" \
+        "name=cache,size=100M,uuid=${uuid_gpt_cache};" \
+        "name=misc,size=512K,uuid=${uuid_gpt_misc};" \
+        "name=frp,size=512K,uuid=${uuid_gpt_frp};" \
+        "name=metadata,size=16M,uuid=${uuid_gpt_metadata};" \
+        "name=userdata,size=-,uuid=${uuid_gpt_userdata};\0"
 
 #endif /* ifndef CONFIG_SPL_BUILD */
 
